72c491bc7bee650e2f6e1d37ff6d2af360cfba4f
PRATHAP4422 <PRATHAP4422@users.noreply.github.com>
2023-11-29 21:37:28 -0600

    Assignment 13 part 5,6 changes

--- models/car.js
+++ models/car.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const carSchema = mongoose.Schema({
 model: String,
 year: Number,
-price: Number
+price: {
+    type:Number,
+    min:1000000,
+    max:10000000
+}
 })
 module.exports = mongoose.model("car",
 carSchema)
